0354f151823660d9f69d7eb81f603b93aa7e298d
CrashedOut93 <crashstocks@gmail.com>
2022-04-16 16:20:23 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,6 +9,17 @@ const db = mysql.createConnection({
     database: 'employeeTest',
 });
 
+app.post('/create', (req, res) => {
+    const name = req.body.name;
+    const age = req.body.age;
+    const country = req.body.country;
+    const position = req.body.position;
+    const wage = req.body.wage;
+
+    db.query('INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)',
+        [name, age, country, position, wage])
+})
+
 app.listen(3001, () => {
     console.log("works")
 })
