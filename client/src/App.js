cb50d017b5f6ddb202973779b6f9cec53c77e990
CrashedOut93 <crashstocks@gmail.com>
2022-04-16 04:17:46 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,35 @@
-
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState('');
+  const [position, setPosition] = useState('');
+  const [wage, setWage] = useState(0);
   return (
     <div className="App">
       <div className='info'>
         <label>Name:</label>
-        <input type="text" />
+          <input type="text"  onChange={(event) => {
+            setName(event.target.value);
+          }} />
         <label>Age:</label>
-        <input type="number" />
+          <input type="number" onChange={(event) => {
+            setAge(event.target.value);
+          }} />
         <label>Country:</label>
-        <input type="text" />
+          <input type="text" onChange={(event) => {
+            setCountry(event.target.value);
+          }} />
         <label>Position:</label>
-        <input type="text" />
+          <input type="text" onChange={(event) => {
+            setPosition(event.target.value);
+          }} />
         <label>Wage (year):</label>
-        <input type="text" />
+          <input type="text" onChange={(event) => {
+            setWage(event.target.value);
+          }} />
         <button>Add Employee</button>
       </div>
     </div>
