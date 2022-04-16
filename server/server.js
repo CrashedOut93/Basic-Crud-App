c1ee4c4df6292a54f585a1a033ce8e532c058e0d
CrashedOut93 <crashstocks@gmail.com>
2022-04-16 16:09:02 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 
-const db = mysql.createConnection();
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: 'thebest1',
+    database: 'employeeTest',
+});
 
 app.listen(3001, () => {
     console.log("works")
