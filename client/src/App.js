1e79dfc169a3b542d3fe41c95da2e0c8f319d213
CrashedOut93 <crashstocks@gmail.com>
2022-04-16 00:52:05 -0500

    Updates

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div>
+      <div className='info'>
         <label>Name:</label>
         <input type="text" />
         <label>Age:</label>
